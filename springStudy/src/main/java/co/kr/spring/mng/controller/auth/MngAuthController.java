d05db988716711807f6c0485183f5ccda8713650
한채원 <@DESKTOP-6KDQK4Q>
2022-05-15 13:12:20 +0900

    2022-05-15

--- springStudy/src/main/java/co/kr/spring/mng/controller/auth/MngAuthController.java
+++ springStudy/src/main/java/co/kr/spring/mng/controller/auth/MngAuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value="/mng/auth/*")
 public class MngAuthController {
 
+	//주석
 	@RequestMapping(value="list")
 	public String list() {
 		return "/mng/auth/list";
